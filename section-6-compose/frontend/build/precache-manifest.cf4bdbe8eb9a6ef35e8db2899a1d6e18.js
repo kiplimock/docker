e762e346ccaa99ea7bf2facaf8c9a1ee1225b5ba
Cedric Kiplimo <kiplimock@gmail.com>
2023-03-26 20:19:29 +0300

    production images with nginx server

---
+++ section-6-compose/frontend/build/precache-manifest.cf4bdbe8eb9a6ef35e8db2899a1d6e18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75eb961d8c2b9aa8bc58",
+    "url": "/static/css/main.51dc05b8.chunk.css"
+  },
+  {
+    "revision": "75eb961d8c2b9aa8bc58",
+    "url": "/static/js/main.75eb961d.chunk.js"
+  },
+  {
+    "revision": "8094871702eebfa8d0be",
+    "url": "/static/js/1.80948717.chunk.js"
+  },
+  {
+    "revision": "4c736754473789d1f9cc",
+    "url": "/static/js/2.4c736754.chunk.js"
+  },
+  {
+    "revision": "642b8d33582affcd04e1",
+    "url": "/static/js/runtime~main.642b8d33.js"
+  },
+  {
+    "revision": "c900b8687fa38532625db54865569c70",
+    "url": "/index.html"
+  }
+];
